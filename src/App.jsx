4a1cd6af4b598d2ce7eab4b7ce1b11f1dd51f93e
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-04-09 15:13:27 +0200

    En masse styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Card from './components/Card'
+import Form from './components/Form'
 import './App.css'
 
 export default function App() {
 
 
   return (
-    <main className="font-mono p-4 dark:bg-slate-900 h-screen">
+    <main className="font-mono p-4 dark:bg-slate-900 min-h-screen max-w-screen-2xl mx-auto">
         <Header title="Frontend links" body="Små links som kunne være brugbare at have"/>
-        <Card title="Title" body="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos eaque fugiat ducimus distinctio!" link="#"/>
+
+        <section className=''>
+            <Form/>
+        </section>
+       
+      
+        <section className='flex relative'>
+            <Card title="Title" body="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos eaque fugiat ducimus distinctio!" link="#"/>
+        </section>
     </main>
   )
 }

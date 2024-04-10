f0659c24a3f20a432efa45eab42e92a6dff94ec5
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-04-10 13:53:09 +0200

    Get requests done

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,23 @@ import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import Card from './components/Card'
 import Form from './components/Form'
+import Cardgrid from './components/Cardgrid.jsx'
 import './App.css'
 
-import {
-    createRecipe,
-    getRecipes,
-    deleteRecipe,
-    updateRecipe,
-  } from "./modules/data.js";
-
-  async funciton (showItems){
-        
-  }
+let headersList = {
+    "Accept": "*/*",
+    "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+    "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxrdG9laWRzdGV3bmpud3JqcnJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3NTE2MzksImV4cCI6MjAyNjMyNzYzOX0.BdU7kexk3VBblD1893zA94IEIxG5aAY59ZilRhjoAc8",
+    "Prefer": "return=representation"
+   }
+   
+   let response = await fetch("https://lktoeidstewnjnwrjrra.supabase.co/rest/v1/frontend-links", { 
+     method: "GET",
+     headers: headersList
+   });
+   
+   let data = await response.json();
+   console.log(data);
 
 export default function App() {
 
@@ -27,9 +32,11 @@ export default function App() {
             </section>
         
         
-            <section className='flex relative'>
+            {/* <section className='flex relative'>
                 <Card title="Title" body="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos eaque fugiat ducimus distinctio!" link="#"/>
-            </section>
+            </section> */}
+
+            <Cardgrid cards={data} />
         </main>
     )
 }
--- src/components/Cardgrid.jsx
+++ src/components/Cardgrid.jsx
@@ -1,14 +1,29 @@
+import { useEffect, useState } from "react";
 import Card from "./Card";
-import { apikey, endpoint } from "./settings.js";
+import { apikey, endpoint } from "../modules/settings";
 
 
-export default function Cardgrid(){
+   
+
+export default function Cardgrid({cards}){
 
 
 
     return (
-        <>
-        <Card />
-        </>
+        <section className='flex relative'>
+
+            {cards.map(card => {
+                return (
+                    <Card key={card.id} title={card.name} body={card.description} link={card.link} />
+                )
+            })
+
+            }
+                    
+                
+
+            
+            
+        </section>
     )
 }
